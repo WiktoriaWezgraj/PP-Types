3eda90e45adc6b3f4d384ffcf180471f8e682b0d
WiktoriaWezgraj <wiktoriawezgraj1@gmail.com>
2024-10-22 20:18:07 +0200

    Lab-2

--- PP-Types/Program.cs
+++ PP-Types/Program.cs
@@ -2,35 +2,69 @@
 
 // Data - do not change it in code!
 string[] names = {
-    "Mickey Mouse", "Minnie Mouse", "Donald Duck", "Goofy", "Pluto", "Daisy Duck", "Simba", "Nala", 
-    "Timon", "Pumbaa", "Mufasa", "Ariel", "Flounder", "Sebastian", "Ursula", "Belle", "Beast", "Gaston", 
-    "Cinderella", "Prince Charming", "Aurora", "Maleficent", "Rapunzel", "Flynn Rider", "Elsa", "Anna", 
+    "Mickey Mouse", "Minnie Mouse", "Donald Duck", "Goofy", "Pluto", "Daisy Duck", "Simba", "Nala",
+    "Timon", "Pumbaa", "Mufasa", "Ariel", "Flounder", "Sebastian", "Ursula", "Belle", "Beast", "Gaston",
+    "Cinderella", "Prince Charming", "Aurora", "Maleficent", "Rapunzel", "Flynn Rider", "Elsa", "Anna",
     "Olaf", "Moana", "Maui", "Hercules"
 };
-
 
 // Print all array elements, *perLine* elements per one line
 // After all elements except last one should be ", " - also on the end of lines.
 // After last element should be ".".
+
 void PrintGroups(string[] t, int perLine)
 {
+    for (int i = 0; i < t.Length; i++)
+    {
+        Console.Write(t[i]);
 
-    // Write required code.
+        if (i != t.Length - 1)
+        {
+            Console.Write(", ");
+        }
+        if ((i + 1) % perLine == 0 && i != t.Length - 1)
+        {
+            Console.WriteLine();
+        }
+    }
 
+    Console.WriteLine(".");
 }
 
-
-// Print all array elements in *perLine* columns.
+//Print all array elements in *perLine* columns.
 // Each column must have given *width* (number of chars).
 // Columns should be separated by "| ".
 // If element is too long it should be trimmed.
 
 void PrintColumns(string[] t, int perLine, int width)
 {
-
-    // Write required code.
-
+    for (int i = 0; i < t.Length; i++)
+    {
+        if (t[i].Length == width)
+        {
+            Console.Write(t[i]);
+        }
+        else if (t[i].Length > width)
+        {
+            Console.Write(t[i].Substring(0, width));
+        }
+        else
+        {
+            Console.Write(t[i].PadRight(width));
+        }
+        if ((i + 1) % perLine == 0)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.Write("|");
+        }
+    }
 }
+
+
+
 
 
 // Test how your functions work. 
@@ -106,7 +140,7 @@ Aurora         | Maleficent     | Rapunzel       | Flynn Rider    | Elsa
 Anna           | Olaf           | Moana          | Maui           | Hercules
 */
 
-Console.WriteLine("\n\nPrintColumns(names, 7, 10):\n");
+Console.WriteLine("\n\nPrintColumns(names, 8, 10):\n");
 PrintColumns(names, 8, 10);
 /*
 Mickey Mou| Minnie Mou| Donald Duc| Goofy     | Pluto     | Daisy Duck| Simba     | Nala
